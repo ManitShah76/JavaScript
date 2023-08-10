6915ec7ac0121e5e9670c485f5e15945e294fceb
ManitShah76 <manitshah07062003@gmail.com>
2023-08-10 22:41:09 +0530

    JavaScript

---
+++ Tasks/Task-1/script.js
@@ -0,0 +1,41 @@
+function handleEvent() {
+    let a = parseInt(document.getElementById("First").value);
+    // console.log(inp_data);
+    
+    let b = parseInt(document.getElementById("Second").value);
+    // console.log(inp_data1);
+    
+    let result = a + b;
+    // console.log(result);
+    alert(result)
+
+    let c = parseInt(document.getElementById("First").value);
+    // console.log(inp_data);
+    
+    let d = parseInt(document.getElementById("Second").value);
+    // console.log(inp_data1);
+    
+    let result1 = c - d;
+    // console.log(result);
+    alert(result1)
+
+    let e = parseInt(document.getElementById("First").value);
+    // console.log(inp_data);
+    
+    let f = parseInt(document.getElementById("Second").value);
+    // console.log(inp_data1);
+    
+    let result2 = e * f;
+    // console.log(result);
+    alert(result2)
+
+    let g = parseInt(document.getElementById("First").value);
+    // console.log(inp_data);
+    
+    let h = parseInt(document.getElementById("Second").value);
+    // console.log(inp_data1);
+    
+    let result3 = g / h;
+    // console.log(result);
+    alert(result3)
+}
